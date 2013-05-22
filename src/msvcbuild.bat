84374313baa9a4f57a573ad15668024debaa3bbb
William Adams <wiladams@microsoft.com>
2013-05-22 06:44:43 -0700

    renamed:    SysInfo_ffi.lua -> core_sysinfo_l1_2_0.lua 	modified:   ../../../tests/test_shutdown.lua

--- src/msvcbuild.bat
+++ src/msvcbuild.bat
@@ -77,7 +77,9 @@
 %LUAC% Win32/apiset/core_processthreads_l1_1_1.lua core_processthreads_l1_1_1.obj
 %LUAC% Win32/apiset/core_profile_l1_1_0.lua core_profile_l1_1_0.obj
 %LUAC% Win32/apiset/core_psapi_l1_1_0.lua core_psapi_l1_1_0.obj
+%LUAC% Win32/apiset/core_shutdown_l1_1_0.lua core_shutdown_l1_1_0.obj
 %LUAC% Win32/apiset/core_string_l1_1_0.lua core_string_l1_1_0.obj
+%LUAC% Win32/apiset/core_sysinfo_l1_2_0.lua core_sysinfo_l1_2_0.obj
 %LUAC% Win32/apiset/core_timezone_l1_1_0.lua core_timezone_l1_1_0.obj
 %LUAC% Win32/apiset/dsrole.lua dsrole.obj
 %LUAC% Win32/apiset/Handle_ffi.lua Handle_ffi.obj
@@ -95,7 +97,6 @@
 %LUAC% Win32/apiset/service_management_l1_1_0.lua service_management_l1_1_0.obj
 %LUAC% Win32/apiset/sspicli.lua sspicli.obj
 %LUAC% Win32/apiset/SubAuth.lua SubAuth.obj
-%LUAC% Win32/apiset/SysInfo_ffi.lua SysInfo_ffi.obj
 %LUAC% Win32/apiset/UMS_ffi.lua UMS_ffi.obj
 %LUAC% Win32/apiset/Util_ffi.lua Util_ffi.obj
 %LUAC% Win32/apiset/WinBer_ffi.lua WinBer_ffi.obj
@@ -103,7 +104,7 @@
 %LUAC% Win32/apiset/wkscli.lua wkscli.obj
 %LUAC% Win32/apiset/wldap32_ffi.lua wldap32_ffi.obj
 
-@set WINCOREAPI=core_console_l1_1_0.obj core_console_l2_1_0.obj core_datetime_l1_1_1.obj core_debug_l1_1_1.obj core_errorhandling_l1_1_1.obj core_file_l1_2_0.obj core_firmware_l1_1_0.obj core_interlocked.obj core_io_l1_1_1.obj core_libraryloader_l1_1_1.obj core_namedpipe_l1_2_0.obj core_processenvironment.obj core_processthreads_l1_1_1.obj core_profile_l1_1_0.obj core_psapi_l1_1_0.obj core_string_l1_1_0.obj core_timezone_l1_1_0.obj dsrole.obj Handle_ffi.obj Heap_ffi.obj httpapi.obj lmcons.obj NTSecAPI.obj power_base_l1_1_0.obj samcli.obj security_base_l1_2_0.obj security_credentials_l1_1_0.obj security_lsalookup_l2_1_0.obj security_sddl_l1_1_0.obj service_core_l1_1_1.obj service_management_l1_1_0.obj sspicli.obj SubAuth.obj SysInfo_ffi.obj UMS_ffi.obj Util_ffi.obj WinBer_ffi.obj WinCon.obj wkscli.obj wldap32_ffi.obj
+@set WINCOREAPI=core_console_l1_1_0.obj core_console_l2_1_0.obj core_datetime_l1_1_1.obj core_debug_l1_1_1.obj core_errorhandling_l1_1_1.obj core_file_l1_2_0.obj core_firmware_l1_1_0.obj core_interlocked.obj core_io_l1_1_1.obj core_libraryloader_l1_1_1.obj core_namedpipe_l1_2_0.obj core_processenvironment.obj core_processthreads_l1_1_1.obj core_profile_l1_1_0.obj core_psapi_l1_1_0.obj core_shutdown_l1_1_0.obj core_string_l1_1_0.obj core_sysinfo_l1_2_0.obj core_timezone_l1_1_0.obj dsrole.obj Handle_ffi.obj Heap_ffi.obj httpapi.obj lmcons.obj NTSecAPI.obj power_base_l1_1_0.obj samcli.obj security_base_l1_2_0.obj security_credentials_l1_1_0.obj security_lsalookup_l2_1_0.obj security_sddl_l1_1_0.obj service_core_l1_1_1.obj service_management_l1_1_0.obj sspicli.obj SubAuth.obj UMS_ffi.obj Util_ffi.obj WinBer_ffi.obj WinCon.obj wkscli.obj wldap32_ffi.obj
 
 
 @rem Create the Win32 specific stuff
--- tests/test_shutdown.lua
+++ tests/test_shutdown.lua
@@ -1,15 +1,21 @@
 -- test_shutdown.lua
 local ffi = require("ffi");
 
-local kernel32 = require("win_kernel32");
 local core_shutdown = require("core_shutdown_l1_1_0");
+local errorhandling = require("core_errorhandling_l1_1_1");
+
+local Token = require("Token");
+
 
 local function test_Shutdown()
+	local token = Token:getProcessToken();
+	token:enablePrivilege(Token.Privileges.SE_SHUTDOWN_NAME);
+	
 	local status = core_shutdown.InitiateSystemShutdownExW(nil, nil,
     	10,false,true,ffi.C.SHTDN_REASON_MAJOR_APPLICATION);
 
 	if status == 0 then
-		return false, kernel32.GetLastError();
+		return false, errorhandling.GetLastError();
 	end
 
 	return true;
