bdf09ca72f6a089c557cea66b095ad6c8c4d9ff3
William Adams <william@nanotechstyles.com>
2013-10-23 09:00:06 -0700

    Added Timer object

---
+++ src/task/Timer.lua
@@ -0,0 +1,62 @@
+local Task = require("IOProcessor")
+ 
+local Timer = {}
+setmetatable(Timer, {
+	__call = function(self, ...)
+		return self:create(...)
+	end,
+});
+
+local Timer_mt = {
+	__index = Timer;
+}
+
+Timer.init = function(self,params)
+	local obj = {
+		Delay = params.Delay;
+		Period = params.Period;
+		Running = false;
+		OnTime = params.OnTime;
+	}
+	setmetatable(obj, Timer_mt);
+
+	obj:start()
+
+	return obj;
+end
+
+Timer.create = function(self, ...)
+	return self:init(...);
+end
+
+Timer.start = function(self)
+	if self.Running then
+		return false, "already running"
+	end
+
+	self.Running = true;
+
+	local function closure()
+		if self.Delay then
+			wait(self.Delay);
+			self.OnTime(self);
+		end
+
+		if not self.Period then
+			return 
+		end
+
+		while self.Running do
+			wait(self.Period)
+			self.OnTime(self);
+		end
+	end
+
+	spawn(closure);
+end
+
+Timer.cancel = function(self)
+	self.Running = false;
+end
+
+return Timer
