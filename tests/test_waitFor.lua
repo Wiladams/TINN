1de90ce07c300c2e6e09dcf75728e3d702f4d99b
William Adams <william@nanotechstyles.com>
2013-12-09 14:32:26 -0800

    new file:   test_waitFor.lua

---
+++ tests/test_waitFor.lua
@@ -0,0 +1,42 @@
+-- test_waitFor.lua
+
+local IOProcessor = require("IOProcessor")
+local Timer = require("Timer")
+
+local count = 0;
+
+
+local goal1 = function()
+  return count >= 5;
+end
+
+local goal2 = function()
+  return count >= 10;
+end
+
+local goal3 = function()
+  return count >= 15;
+end
+
+local finalGoal = function()
+  waitFor(goal1)
+print("Goal 1 Attained.")
+  waitFor(goal2)
+print("Goal 2 Attained..")
+  waitFor(goal3)
+print("Goal 3 Attained...")
+print("All Goals Attained!!")
+
+  -- Once all the goals have been met
+  -- stop the scheduler.
+  stop();
+end
+
+-- setup a timer to increment the count
+-- every 500 milliseconds
+local incrTimer = Timer({Period = 500; OnTime = function(timer) count = count+1 end})
+
+spawn(finalGoal)	
+
+run()
+
